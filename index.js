7ffe665df0766ed53435052ab1d235c4472190f1
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-23 22:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const element = document.getElementById('dodger');
 
-document.addEventListener('keydown', (event) => {
-  console.log(event);
-  if (event.which === 37) {
-    // '180px'
-    let previousLeft = element.style.left;
-    // 180
-    let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10);
-    if (previousLeftNumber > 0) {
-    let newLeftNumber = (previousLeftNumber - 15) + "px";
-    element.style.left = newLeftNumber;
-    }
-  }
-})
+// document.addEventListener('keydown', (event) => {
+//   console.log(event);
+//   if (event.which === 37) {
+//     // '180px'
+//     let previousLeft = element.style.left;
+//     // 180
+//     let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10);
+//     if (previousLeftNumber > 0) {
+//     let newLeftNumber = (previousLeftNumber - 15) + "px";
+//     element.style.left = newLeftNumber;
+//     }
+//   }
+// })
 
 function moveDodgerRight(event) {
   document.addEventListener('keydown', (event) => {
