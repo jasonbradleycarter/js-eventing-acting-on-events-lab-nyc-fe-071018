ce1321bec7f2f3146dab880604fbe2612792275c
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-23 22:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const element = document.getElementById('dodger');
 
 document.addEventListener('keydown', (event) => {
   console.log(event);
-  if (event.which === 37 && element.style.left > 0) {
+  if (event.which === 37) {
     // '180px'
     let previousLeft = element.style.left;
+    if previousLeft > 0) {
     // 180
     let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10);
     let newLeftNumber = (previousLeftNumber - 15) + "px";
     element.style.left = newLeftNumber;
+   }
   }
 })
